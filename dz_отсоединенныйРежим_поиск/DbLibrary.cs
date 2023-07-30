7a39e582605a50d09b37438f5a803a6d2743d360
Радченко Катерина <ketrinradchenko@gmail.com>
2023-07-30 06:56:56 +0300

    Add project files.

---
+++ dz_отсоединенныйРежим_поиск/DbLibrary.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Linq;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace dz_отсоединенныйРежим_поиск
+{
+    internal class DbLibrary : DataContext
+    {
+        // создаем объект Соединение
+        SqlConnection connection = new SqlConnection();
+        // создаем объект класса DataSet - контейнер для хранения
+        // данных из БД 
+        DataSet dataSet = new DataSet();
+        // создаем объект класса SqlDataAdapter - для работы
+        // отсоединенного режима
+        SqlDataAdapter dataAdapter = new SqlDataAdapter();
+       
+        public DbLibrary() : base(ConfigurationManager.ConnectionStrings["library"].ConnectionString)
+        {
+            // синициализация свойства ConnectionString подключения
+            // строкой подключения из конфигурационного файла
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["library"].ConnectionString;
+        }
+
+        // метод, который возвращает таблицу с данными DataTable
+        public DataTable MyTable()
+        {
+            dataAdapter.Fill(dataSet);     // получение данных из БД
+                                           // Fill(dataSet) выполняет запрос, занесенный 
+                                           // свойство SelectCommand и заносит данные в dataSet
+            if (dataSet.Tables.Count== 0)  // если таблице не загружены (нет данных в БД)
+                return null;               // возвращаем пустую ссылку
+            return dataSet.Tables[0];      // иначе возвращаем таблицу с данными
+        }
+
+        // метод для поиска книг по автору
+        public void SearchByAuthor(string surname)
+        {
+            // создание строки-запроса в БД
+            string cmdText = $"select * from Authors where LastName like'%{surname}%'";
+            // создание объекта-команды, инициализация его подключением и запросом
+            SqlCommand cmd = new SqlCommand(cmdText, connection);
+            // инициализируем свойство SelectCommand класса dataAdapter
+            dataAdapter.SelectCommand = cmd;
+        }
+
+        // метод для поиска книг по названию
+        public void SearchByTitle(string title)
+        {
+            // создание строки-запроса в БД
+            string cmdText = $"select * from Books where Name like'%{title}%'";
+            // создание объекта-команды, инициализация его подключением и запросом
+            SqlCommand cmd = new SqlCommand(cmdText, connection);
+            // инициализируем свойство SelectCommand класса dataAdapter
+            dataAdapter.SelectCommand = cmd;
+            
+        }
+
+        // метод для поиска книг по категории
+        public void SearchByCategory(string category)
+        {
+            // создание строки-запроса в БД
+            string cmdText = $"select * from Books, Categories " +
+                $"where Id_Category = Categories.Id and Categories.Name like '%{category}%'";
+            // создание объекта-команды, инициализация его подключением и запросом
+            SqlCommand cmd = new SqlCommand(cmdText, connection);
+            // инициализируем свойство SelectCommand класса dataAdapter
+            dataAdapter.SelectCommand = cmd;
+        }
+    }
+}
---
+++ dz_отсоединенныйРежим_поиск/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dz_отсоединенныйРежим_поиск
+{
+    public partial class Form1 : Form
+    {
+        DbLibrary db = null;
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Выберите, по какой категории будет осуществляться поиск:";
+            label2.Text = "Введите параметр поиска:";
+            button1.Text = "Найти";
+        }
+
+        // обработчик события - клака по кнопке Найти
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // в зависимости от выбранного пункта в КомбоБокс
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:  // если выбран Поиск по автору
+                    using (db = new DbLibrary())
+                    {
+                        dataGridView1.DataSource = null;   // очищаем таблицу от предыдущих данных
+                        db.SearchByAuthor(textBox1.Text);  // вызов метода Поиск по автору
+                        DataTable myTable = db.MyTable();  // создаем новый объект myTable и заполняем данными
+                        dataGridView1.DataSource           // создаем привязку для dataGridView1 - источник 
+                            = myTable.DefaultView;         // данных это объект myTable
+                    }                 
+                    break;
+                case 1:  // если выбран поиск по Названию, аналогично case 0
+                    using (db = new DbLibrary())
+                    {                      
+                        dataGridView1.DataSource = null;
+                        db.SearchByTitle(textBox1.Text);
+                        DataTable myTable1 = db.MyTable();
+                        dataGridView1.DataSource = myTable1.DefaultView;
+                    }                       
+                    break;
+                case 2:  // если выбран поиск по Категории, аналогично case 0
+                    using (db = new DbLibrary())
+                    {
+                        dataGridView1.DataSource = null;
+                        db.SearchByCategory(textBox1.Text);
+                        DataTable myTable1 = db.MyTable();
+                        dataGridView1.DataSource = myTable1.DefaultView;
+                    }
+                    break;
+            }
+        }
+    }
+}
